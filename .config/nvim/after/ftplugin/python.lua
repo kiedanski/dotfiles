0e8dfec6a68e2415f6852e39ddbe359b503af303
Diego Kiedanski <dkiedanski@tryolabs.com>
2023-06-13 13:59:30 -0300

    update python

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,6 +1,7 @@
 local set = vim.opt
 
-set.wrap = false
+set.wrap = true
+-- set.linewidth = 80
 
 set.tabstop = 4
 set.softtabstop = 4
@@ -10,3 +11,16 @@ set.expandtab = true
 vim.g.slime_python_ipython = true
 
 -- require'lspconfig'.pyright.setup{}
+--
+-- Custom folding configuration for Python files
+local function custom_python_folding()
+  -- Set the foldmethod to marker
+  vim.wo.foldmethod = 'marker'
+  
+  -- Set the foldmarker to use the custom marker
+  vim.wo.foldmarker = '## %,## %'
+end
+
+-- Enable custom folding for Python files
+vim.cmd('autocmd FileType python lua custom_python_folding()')
+
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,10 @@ require "autocomplete"
 
 vim.g.slime_target = "tmux"
 vim.g["slime_default_config"] = {socket_name = "default", target_pane = "{last}"}
+
+-- Set background color for folded regions
+vim.cmd('highlight Folded guibg=DarkGrey guifg=White')
+
+-- Set the character used to separate folded regions
+vim.o.fillchars = 'fold:─'
+
